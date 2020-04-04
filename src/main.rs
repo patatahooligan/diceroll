acb6765c68ce9651974ebfcca2815936152ca507
patatahooligan <filwn.oikonomou@gmail.com>
2020-04-04 23:17:09 +0300

    Print all die results, not just sum

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,24 @@ use diceroll::*;
 fn main() {
     for arg in env::args().skip(1) {
         let mut dice_roll = parse_roll_string(arg);
+        assert!(!dice_roll.dice.is_empty());
+
         roll_dice(&mut dice_roll);
+
+        print!("{} (d{})", dice_roll.dice[0].roll, dice_roll.dice[0].max);
+
+        for die in dice_roll.dice.iter().skip(1) {
+            print!(" + {} (d{})", die.roll, die.max);
+        }
+
+        if dice_roll.modifier != 0 {
+            print!(" + {}", dice_roll.modifier);
+        }
 
         let result =
             dice_roll.dice.iter().fold(0, |sum, die| sum + die.roll) +
             dice_roll.modifier;
 
-        println!("{}", result);
+        println!(" = {}", result);
     }
 }
