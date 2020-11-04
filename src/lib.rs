db5bbab00391020de91b233ec58ba055eff7e266
patatahooligan <filwn.oikonomou@gmail.com>
2020-11-04 02:04:58 +0200

    Add negative roll/modifier support

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,47 @@
 use rand::prelude::*;
 
+#[derive(Copy, Clone)]
+pub enum Sign {
+    Plus,
+    Minus,
+}
+
+impl Sign {
+    fn from_char(c: char) -> Result<Sign, &'static str> {
+       match c {
+           '+' => Ok(Sign::Plus),
+           '-' => Ok(Sign::Minus),
+           _ => Err("Sign character should be '+' or '-'"),
+       }
+    }
+
+    pub fn to_char(self) -> char {
+        match self {
+            Sign::Plus => '+',
+            Sign::Minus => '-',
+        }
+    }
+
+    pub fn to_multiplier(self) -> i32 {
+        match self {
+            Sign::Plus => 1,
+            Sign::Minus => -1,
+        }
+    }
+}
+
 pub struct SingleDieRoll {
-    pub max: u32,
-    pub roll: u32,
+    pub max: i32,
+    pub roll: i32,
+    pub sign: Sign,
 }
 
 pub struct DiceRoll {
     pub dice: Vec<SingleDieRoll>,
-    pub modifier: u32,
+    pub modifier: i32,
 }
 
-fn roll_single_die(max: u32) -> u32 {
+fn roll_single_die(max: i32) -> i32 {
     // gen_range takes [min, max) but dice are usually notated as 1dX <-> (0, X]
     // so increment the max
     thread_rng().gen_range(1, max + 1)
@@ -26,24 +57,46 @@ pub fn parse_roll_string(mut roll_string: String) -> DiceRoll {
     // Expect the notation "1d20 + 2d8 + 3, ..."
     remove_whitespace(&mut roll_string);
 
-    // Here single_roll means roll in the d&d so it might contain more than one
-    // die and modifiers
+    // To make parsing more uniform, add a '+' to the beginning of the string if
+    // no sign exists there.
+    if !roll_string.starts_with(&['+', '-'][..]) {
+        roll_string.insert(0, '+');
+    }
+
+    // Do the parsing in two steps, first split on '+'/'-' (keeping the sign),
+    // then parse the substrings.
+    let mut roll_substrings = Vec::new();
+    while let Some(next_sign_pos) = roll_string.rfind(&['+', '-'][..]) {
+        roll_substrings.push(
+            roll_string.split_off(next_sign_pos));
+    }
+
+    roll_substrings.reverse();
+
     let mut die_roll = DiceRoll {dice: Vec::new(), modifier: 0};
-    for part in roll_string.split('+') {
+    for substring in roll_substrings {
         // Rolls are '{x}d{y}', modifiers are just '{x}'
-        match part.contains('d') {
+
+        match substring.contains('d') {
             true => {
-                let elements: Vec<&str> = part.split('d').collect();
+                let sign =
+                    Sign::from_char(substring.as_bytes()[0] as char).unwrap();
+                let substring = &substring[1..];
+                let elements: Vec<&str> = substring.split('d').collect();
                 assert_eq!(elements.len(), 2);
-                let amount = elements[0].parse::<u32>().unwrap();
-                let max = elements[1].parse::<u32>().unwrap();
+                let amount = elements[0].parse::<i32>().unwrap();
+                let max = elements[1].parse::<i32>().unwrap();
 
                 for _i in 0..amount {
-                    die_roll.dice.push(SingleDieRoll{max, roll: 0});
+                    die_roll.dice.push(
+                        SingleDieRoll{
+                            max,
+                            roll: 0,
+                            sign});
                 }
             }
             false => {
-                die_roll.modifier += part.parse::<u32>().unwrap();
+                die_roll.modifier += substring.parse::<i32>().unwrap();
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,21 @@ fn main() {
         print!("{} (d{})", dice_roll.dice[0].roll, dice_roll.dice[0].max);
 
         for die in dice_roll.dice.iter().skip(1) {
-            print!(" + {} (d{})", die.roll, die.max);
+            print!(
+                " {0} {1} (d{2})",
+                die.sign.to_char(), die.roll, die.max);
         }
 
-        if dice_roll.modifier != 0 {
+        if dice_roll.modifier > 0 {
             print!(" + {}", dice_roll.modifier);
+        }
+        else if dice_roll.modifier < 0 {
+            print!(" - {}", -dice_roll.modifier);
         }
 
         let result =
-            dice_roll.dice.iter().fold(0, |sum, die| sum + die.roll) +
+            dice_roll.dice.iter().fold(
+                0, |sum, die| sum + die.roll * die.sign.to_multiplier()) +
             dice_roll.modifier;
 
         println!(" = {}", result);
