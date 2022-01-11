431b5fad5a1634cabe3f630ebf91edc79947df8a
patatahooligan <filwn.oikonomou@gmail.com>
2022-01-11 23:46:08 +0200

    Add -h flag

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,25 @@ use std::env;
 
 use diceroll::*;
 
+fn print_help() {
+    print!("Usage: {} ", env::args().next().unwrap_or("".to_string()));
+    println!("DICEROLL [DICEROLL [...]]");
+    println!();
+    println!("You can specify any number space-separated dicerolls. Each ");
+    println!("one can be comprised of multiple dice using the conventional ");
+    println!("D&D syntax. Examples:");
+    println!();
+    println!("  3d20: the sum of 3 d20 rolls");
+    println!("  1d20+1d4: the sum of a d20 and a d4");
+    println!("  2d8-1: the sum of 2 d8 rolls with a -1 modifier")
+}
+
 fn main() {
+    if env::args().skip(1).any(|x| x == "-h") {
+        print_help();
+        return;
+    }
+
     for arg in env::args().skip(1) {
         let mut dice_roll = parse_roll_string(arg);
         assert!(!dice_roll.dice.is_empty());
